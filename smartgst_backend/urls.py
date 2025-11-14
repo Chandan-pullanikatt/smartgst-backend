749abae414f9476acd2d87466095c92248abbde4
chandan <chandanpullanikatt@gmail.com>
2025-11-14 18:34:21 +0530

    Fix Swagger login 404 by adding auth URLs

--- smartgst_backend/urls.py
+++ smartgst_backend/urls.py
@@ -36,6 +36,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 
     # ✅ Swagger Documentation
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
